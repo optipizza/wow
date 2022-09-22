ab7deac08fdf15c83ec5b3edf8411217e5ddeed7
optipizza <114149190+optipizza@users.noreply.github.com>
2022-09-22 12:39:18 +0200

    Add files via upload

---
+++ precache-manifest.aa46a5a5802af033dc7fd9fd1ddc86e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3583858acb98d38453d293192b810be1",
+    "url": "/wow/index.html"
+  },
+  {
+    "revision": "9c7e1c5ef7724b58c299",
+    "url": "/wow/static/css/main.dc56fa15.chunk.css"
+  },
+  {
+    "revision": "643cc5d57747d2881da3",
+    "url": "/wow/static/js/2.744d4cea.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/wow/static/js/2.744d4cea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c7e1c5ef7724b58c299",
+    "url": "/wow/static/js/main.64831f4e.chunk.js"
+  },
+  {
+    "revision": "2e1a8e96203ab6b4220a",
+    "url": "/wow/static/js/runtime-main.0817d79c.js"
+  }
+]);
